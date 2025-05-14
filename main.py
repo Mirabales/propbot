6c37eb6b9ef35b84993ca8b0b8fc6c204cd276ea
Mirabales <mirabalmisael@gmail.com>
2025-05-14 15:17:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
-
 import os
 
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=CHAT_ID, text="📬 Hola Juan Pérez, tu renta mensual de RD$25,000 vence el día 5. — Tu asistente PropBot 🤖")
+    user_id = update.effective_chat.id  # 🔥 Esto agarra el ID de quien escribe
+    await context.bot.send_message(
+        chat_id=user_id,
+        text="📬 Hola, este es tu recordatorio de renta. — Tu asistente PropBot 🤖"
+    )
 
 if __name__ == "__main__":
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-
     app.run_polling()
+
